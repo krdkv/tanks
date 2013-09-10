2bac5f3a3b6a9b5564e909d2d07ffc551e801b30
Nick Kardakov <Nikita.Kardakov@gmail.com>
2013-09-10 09:50:16 +0200

    Simple interaction

--- example.js
+++ example.js
@@ -1,26 +1,18 @@
 var ws = require('websocket.io')
   , server = ws.listen(3000)
 
-var clientArray = new Array();
-
-function printClientStatus() {
-    console.log("Number of clients " + clientArray.length);
-}
-
 server.on('connection', function (client) {
 
-  clientArray.push(client);
-  printClientStatus();
-          
-  client.on('message', function () {
+  console.log("Client connected. Now have " + server.clientsCount + " clients");
 
+  client.send(JSON.stringify({"method":"hello"}));
+
+  client.on('message', function (data) {
+        console.log("From client " + data);
   });
   
-  client.on('close', function (client) {
-
-  console.log("close " + client);
-
-});
-          
+  client.on('close', function () {
+      console.log("Client diconnected. Now have " + server.clientsCount + " clients");
+  });
           
 });
