5bbcdc4126749ec5804e6569ec3401c4af088d01
Nick Kardakov <Nikita.Kardakov@gmail.com>
2013-09-10 14:13:13 +0200

    Simple game and tournament

--- server.js
+++ server.js
@@ -1,18 +1,85 @@
 var ws = require('websocket.io')
 , server = ws.listen(3000);
 
+var TOURNAMENT_EXPECTED_PLAYERS_COUNT = 3;
+
+
+// =================================================== GAME ===================================================
+
+function Game() {
+    this.players = new Array();
+}
+
+Game.prototype.addPlayer = function(player) {
+    this.players.push(player);
+}
+
+Game.prototype.play = function() {
+    var playersArray = new Array();
+    for ( var player in this.players ) {
+        playersArray.push(this.players[player]["nickname"]);
+    }
+    broadcast({"method":"endGame", "winner":this.players[Math.floor((Math.random()*this.players.length))]["nickname"], "players":playersArray});
+}
+
+
+
+// =================================================== TOURNAMENT ===================================================
+
+function Tournament() {
+    this.players = new Array();
+    this.games = new Array();
+}
+
+Tournament.prototype.addPlayer = function(player) {
+    this.players.push(player);
+    if ( this.players.length == TOURNAMENT_EXPECTED_PLAYERS_COUNT ) {
+        this.start();
+    }
+}
+
+Tournament.prototype.start = function() {
+    
+    for ( var i = 0; i < this.players.length; ++i ) {
+        for ( var j = 0; j < this.players.length; ++j ) {
+            if ( j <= i ) {
+                continue;
+            }
+            var newGame = new Game();
+            newGame.addPlayer(this.players[i]);
+            newGame.addPlayer(this.players[j]);
+            this.games.push(newGame);
+        }
+    }
+    
+    for ( var i = 0; i < this.games.length; ++i ) {
+        this.games[i].play();
+    }
+}
+
+// =================================================== SERVER ===================================================
+
+var tournament  = new Tournament();
+
 server.on('connection', function (client) {
-
-  console.log("Client connected. Now have " + server.clientsCount + " clients");
-
-  client.send(JSON.stringify({"method":"hello"}));
-
+        
+  client.send(JSON.stringify({"method" : "hello"}));
+  
   client.on('message', function (data) {
-        console.log("From client " + data);
+        var response = JSON.parse(data);
+        if ( response["method"] == "name" ) {
+            client["nickname"] = response["data"];
+            tournament.addPlayer(client);
+        }
   });
   
   client.on('close', function () {
-      console.log("Client diconnected. Now have " + server.clientsCount + " clients");
   });
           
 });
+
+function broadcast(jsObject) {
+    for ( var client in server.clients ) {
+        server.clients[client].send(JSON.stringify(jsObject));
+    }
+}
