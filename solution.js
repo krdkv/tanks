f865115f60aab67180a31c4d244c8f5d4e46dfb6
gabi <gabi@ger-dell.(none)>
2013-09-11 23:56:39 +0200

    Basic up-down movements of tanks.

--- solution.js
+++ solution.js
@@ -1,5 +1,8 @@
+var myName = Math.random().toString(36).substring(7);
+var dir = "up";
+
 function getName() {
-    return "Super team";
+    return myName;
 }
 
 // players - Array of names
@@ -9,7 +12,43 @@ function getName() {
 // map["height"]
 
 function startGame(players, map) {
-    
+}
+
+function newTurn(map) {
+    var myTank;
+    for (var i=0; i<map.tanks.length; i++) {
+        tank = map.tanks[i];
+        console.log(tank);
+        if (tank["nickname"] == myName) {
+            myTank = tank;
+            break;
+        }
+    }
+    var myX = myTank["x"];
+    var myY = myTank["y"];
+    console.log("My position: " + myX + " " + myY);
+    var dX = 0;
+    var dY = 0;
+    if (dir == "up") {
+        if (myY > 0) {
+            dY = -1;
+        }
+        else {
+            dY = 1;
+            dir = "down";
+        }
+    }
+    else if (dir == "down") {
+        if (myY < map["height"]-1) {
+            dY = 1;
+        }
+        else {
+            dY = -1;
+            dir = "up";
+        }
+    }
+    console.log("My action: " + dX + " " + dY);
+    return {"deltaX": dX, "deltaY": dY};
 }
 
 // winner - String
